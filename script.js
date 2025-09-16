f47bb60472f4ece6efc1ecb0e23b8bf80c91443d
stepan <selestepan@gmail.com>
2025-09-16 05:48:39 +0000

    svaga

--- script.js
+++ script.js
@@ -3,17 +3,29 @@ const trackContainer = document.querySelector(".tracks-list")
 async function trackData() {
     const res1 = await fetch("https://kitek.ktkv.dev/songs.json")
     const res2 = await res1.json()
+    let totalDuration = 0
     for (let i = 0; i < res2.length; i++) {
-        const trackItemLi = document.createElement("li")
-        trackItemLi.classList.add("trackItem")
-        trackItemLi.textContent = res2[i].track.name
-        const trackNumberDiv = document.createElement("div")
-        trackNumberDiv.classList.add("track-number")
-        const trackMainDiv = document.createElement("div")
-        trackMainDiv.classList.add("track-main")
-        const trackNameDiv
-        trackName.appendChild(trackItemLi)
+        statsContainer.textContent = `Треков: ${res2.length}`
+        totalDuration += res2[i].track.duration_ms
+        const trackItem = document.createElement("li")
+        trackItem.classList.add("track-item")
+        const trackNumber = document.createElement("div")
+        trackNumber.classList.add("track-number")
+        trackNumber.textContent = i+1
+        trackItem.appendChild(trackNumber)
+        trackContainer.appendChild(trackItem)
+        const trackMain = document.createElement("div")
+        trackMain.classList.add("track-main")
+        trackMain.textContent = i+1
+
+
+        
     }
+    let totalSeconds = Math.floor(totalDuration / 1000)
+    let minutes = Math.floor(totalSeconds / 60)
+    let seconds = totalSeconds % 60
+    let durationString = `${minutes}:${seconds}`
+    statsContainer.textContent = `Общее количество треков: ${res2.length}, общая длительность: ${durationString}`
 }
 
 trackData()
